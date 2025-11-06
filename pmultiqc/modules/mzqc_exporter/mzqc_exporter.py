ecd782f13cd39aa34dc2d3f0e3ed2ab433cc22d0
julianu <julian.uszkoreit@rub.de>
2025-11-06 16:57:51 +0000

    removed obsolete code

--- pmultiqc/modules/mzqc_exporter/mzqc_exporter.py
+++ pmultiqc/modules/mzqc_exporter/mzqc_exporter.py
@@ -2,12 +2,12 @@ import json
 import os
 from datetime import datetime
 from pathlib import Path
-from typing import Any, Dict, List, Literal, Mapping, NewType, Optional, Sequence, Tuple, TypedDict, Union, cast
+from typing import Mapping, Sequence, Union
 
 from mzqc import MZQCFile as qc
 
 from multiqc import config
-from multiqc.plots.bargraph import InputDatasetT, CatName, InputCategoriesT, BarPlot, BarPlotConfig
+from multiqc.plots.bargraph import InputDatasetT
 
 # define common metric units
 metric_unit_count = {"unit_accession": "UO:0000189", "unit_name": "count unit"}
@@ -22,63 +22,6 @@ class MzQCExporterModule():
         self.run_quality_metrics = {}   # intermediate store for lists of run_qualities per run
         self.run_metadata = {}          # intermediate store for metadata per run, each run has a dict with the keys 'input_files' and 'analysis_software', values are lists of these
 
-
-    def export_mzqc(self, mzqc_data) -> bool | None:
-        """
-        Exports MZQC data to a MZQC file format.
-
-        This function takes structured MZQC data and writes it to a file in the
-        MZQC (Mass Spectrometry QC) format. The MZQC format is designed to
-        capture quality control metrics from mass spectrometry experiments in a
-        standardized way that can be shared and processed by various tools.
-
-        Args:
-            mzqc_data (dict): A dictionary containing the mzQC data structure.
-                              This must include 'run_qualities' and 'set_qualities',
-                              which are lists of QualityMetric objects
-
-        Returns:
-            bool | None: Returns True if the export was successful, False if it failed,
-                         or None if no export was performed due to invalid input.
-
-        Example:
-            >>> exporter = MzQCExporterModule()
-            >>> qm = qc.QualityMetric( ... )
-            >>> mzqc_data = {
-            ...     "run_qualities": [qm],
-            ...     "set_qualities": [],
-            ... }
-            >>> exporter.export_mzqc(mzqc_data)
-
-        Note:
-            This function requires the 'mzqc' library to be installed and properly
-            configured. The input data structure must conform to the MZQC schema
-            specification.
-        
-        """
-        self.log.info("Starting mzQC export ...")
-
-        cv_ms = qc.ControlledVocabulary(
-            name="Proteomics Standards Initiative Mass Spectrometry Ontology",
-            version="4.1.212",
-            uri="https://github.com/HUPO-PSI/psi-ms-CV/blob/master/psi-ms.obo")
-
-        mzqc = qc.MzQcFile(version="1.0.0",
-                           creationDate=datetime.now().isoformat(),
-                           runQualities=mzqc_data["run_qualities"],
-                           setQualities=mzqc_data["set_qualities"],
-                           controlledVocabularies=[cv_ms])
-
-        output_dir = Path(config.output_dir)
-        if output_dir is not None:
-            # TODO: set the pmultiqc-output by parameters
-            mzqc_filename = os.path.join(output_dir, "pmultiqc.mzqc")
-            with open(mzqc_filename, "w") as mzqc_file:
-                mzqc_file.write(json.dumps(json.loads(qc.JsonSerialisable.to_json(mzqc)), indent=2))
-    
-        self.log.info(f"Done exporting mzQC to {mzqc_filename}")
-        return True
-    
  
     def create_export(self):
         """
